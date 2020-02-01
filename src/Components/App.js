ef1b112d06a0e1471a21e6ad689f795d1464403a
marcoazzurrini <marco.azzurrini.art@gmail.com>
2020-02-01 21:20:40 +0100

    adjust media queries

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,21 +1,19 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import Header from './Header';
-import Form from './Form';
-import Footer from './Footer';
-import ProgressBar from './ProgressBar';
+import Header from "./Header";
+import Form from "./Form";
+import ProgressBar from "./ProgressBar";
 
 export default class App extends Component {
-    render() {
-        return (
-            <div className="container">
-                <div className="calculator">
-                    <ProgressBar />
-                    <Header />
-                    <Form />
-                    <Footer />
-                </div>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="container">
+        <div className="calculator">
+          <ProgressBar />
+          <Header />
+          <Form />
+        </div>
+      </div>
+    );
+  }
 }
--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,31 +1,99 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import Questions from "../Questions";
+import GoBackSVG from "../img/left-chevron.svg";
 
 export default class Form extends Component {
+  state = {
+    revenue: "",
+    saleWorth: "",
+    sessionConversion: "",
+    outreachConversion: "",
+    count: 0
+  };
 
-    state = { revenue: '', saleWorth: '' };
+  handleChange = e => {
+    switch (e.target.id) {
+      case "revenue":
+        this.setState({ revenue: e.target.value });
+        break;
+      case "saleWorth":
+        this.setState({ saleWorth: e.target.value });
+        break;
+      case "sessionConversion":
+        this.setState({
+          sessionConversion: e.target.value
+        });
+        break;
+      case "outreachConversion":
+        this.setState({
+          outreachConversion: e.target.value
+        });
+        break;
+      default:
+        console.log("something went wrong");
+    }
+  };
 
-    handleChange = e => {
-        switch(e.target.id) {
-            case 'revenue': this.setState({ revenue: e.target.value }); break;
-            case 'saleWorth': this.setState({ saleWorth: e.target.value }); break;
-            default: console.log('something went wrong');
+  updateCount = e => {
+    switch (e.dataset.action) {
+      case "next":
+        if (this.state.count < Questions.length - 1) {
+          let newCount = this.state.count + 1;
+          this.setState({ count: newCount });
         }
+        break;
+
+      case "previous":
+        if (this.state.count > 0) {
+          let newCount = this.state.count - 1;
+          this.setState({ count: newCount });
+        }
+        break;
+
+      default:
+        console.log("something went wrong");
     }
+  };
 
-    render() {
-        return (
-            <form className="calculator__form">
+  render() {
+    return (
+      <form className="calculator__form">
+        {Questions[this.state.count].questions.map((question, index) => {
+          return (
+            <div key={index}>
+              <label className="calculator__form--label">
+                {question.label}
+              </label>
+              <input
+                className="calculator__form--input"
+                required
+                placeholder={question.placeholder}
+                type="number"
+                id={question.tag}
+                value={this.state.revenue}
+                onChange={this.handleChange}
+              />
+            </div>
+          );
+        })}
+        <div className="calculator__footer">
+          <img
+            className="calculator__form--previous"
+            src={GoBackSVG}
+            alt="previous"
+            data-action="previous"
+            onClick={this.updateCount}
+          />
 
-               <label className="calculator__form--label">How much do you wanna make in a year</label>
-               <input className="calculator__form--input" required placeholder="Desired yearly revenue" type="number" 
-                id="revenue" value={this.state.revenue} onChange={this.handleChange}
-               />
-
-               <label className="calculator__form--label">How much do you charge for your product?</label>
-               <input className="calculator__form--input" required placeholder="Single sale net worth" type="number" 
-                id="saleWorth" value={this.state.saleWorth} onChange={this.handleChange}
-               />
-            </form>
-        )
-    }
+          <button
+            onClick={this.updateCount}
+            data-action="next"
+            className="calculator__footer--btn"
+          >
+            Next
+          </button>
+        </div>
+      </form>
+    );
+  }
 }
