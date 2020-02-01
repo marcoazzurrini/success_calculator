6623ef927164c50e62fc145f3876ac2ad24d2478
marcoazzurrini <marco.azzurrini.art@gmail.com>
2020-02-01 21:29:36 +0100

    edit header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class Header extends Component {
-    render() {
-        return (
-            <div className="calculator__header">
-                <h1 className="calculator__header--title">FREELANCING MATH CALCULATOR</h1>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="calculator__header">
+        <h1 className="calculator__header--title">Freelancing math.</h1>
+      </div>
+    );
+  }
 }
